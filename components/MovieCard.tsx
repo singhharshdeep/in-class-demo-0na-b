327e5f09181d65d9d33c00ead170d2c3cf68e9a1
Harshdeep Singh <singh.harshdeep@live.in>
2025-12-08 14:21:08 -0500

    Adds api

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,34 +1,38 @@
-import { useContext } from "react";
-import { Movie } from "../types";
+import { ReactNode, useContext } from "react";
+import { MovieResponse } from "../types";
 import { AuthContext } from "@/context/AuthContext";
 import { useRouter } from "next/router";
 
 type MovieCardProps = {
-  movie: Movie;
-  addToFavorites: (movie: Movie) => void;
-  removeFromFavorites: (movie: Movie) => void;
-  isFavorite?: boolean;
+  movie: MovieResponse;
+  children: ReactNode;
 };
 
-export function Poster({ poster_path, title }) {
+export function Poster({
+  poster_path,
+  title,
+}: {
+  poster_path: string;
+  title: string;
+}) {
   return (
     <img
-      src={`https://image.tmdb.org/t/p/w300${poster_path}`}
+      src={poster_path}
       alt={title}
       className="rounded-t-2xl w-[300px] h-[300px]"
     />
   );
 }
 
-export function MovieTitle({ title }) {
+export function MovieTitle({ title }: { title: string }) {
   return <div>{title}</div>;
 }
 
-export function MovieRelease({ release_date }) {
+export function MovieRelease({ release_date }: { release_date: string }) {
   return <div>{release_date}</div>;
 }
 
-export function MovieRating({ rating }) {
+export function MovieRating({ rating }: { rating: string }) {
   return <div>Rating: {rating}</div>;
 }
 
@@ -37,6 +41,11 @@ export function FavoritesButton({
   isFavorite,
   removeFromFavorites,
   addToFavorites,
+}: {
+  movie: MovieResponse;
+  isFavorite: boolean;
+  removeFromFavorites: (movie: MovieResponse) => void;
+  addToFavorites: (movie: MovieResponse) => void;
 }) {
   const { isLoggedIn } = useContext(AuthContext);
 
@@ -53,45 +62,21 @@ export function FavoritesButton({
   ) : null;
 }
 
-export function MovieDetails({ children }) {
+export function MovieDetails({ children }: { children: ReactNode }) {
   return <div className="flex flex-col items-center text-xl">{children}</div>;
 }
 
 export default function MovieCard({ movie, children }: MovieCardProps) {
   const router = useRouter();
   function handleClick() {
-    router.push(`/movies/${movie.id}`);
+    router.push(`/movies/${movie.imdbID}`);
   }
   return (
     <div
       className="transition hover:scale-105 border border-white mt-4 w-[300px] h-[450px] rounded-2xl"
       onClick={handleClick}
     >
-<<<<<<< Updated upstream
-      <img
-        src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`}
-        alt={movie.title}
-        className="rounded-t-2xl w-[300px] h-[300px]"
-      />
-      <div className="flex flex-col items-center text-xl">
-        <div>{movie.title}</div>
-        <div>{movie.release_date}</div>
-        <div>Rating: {movie.vote_average}</div>
-        {isLoggedIn ? (
-          <button
-            className="hover:bg-gray-400 border border-white p-3 rounded-4xl"
-            onClick={(e) => {
-              e.stopPropagation();
-              isFavorite ? removeFromFavorites(movie) : addToFavorites(movie);
-            }}
-          >
-            {isFavorite ? "Remove From Favorites" : "Add To Favorite"}
-          </button>
-        ) : null}
-      </div>
-=======
       {children}
->>>>>>> Stashed changes
     </div>
   );
 }
--- context/MoviesContext.tsx
+++ context/MoviesContext.tsx
@@ -1,15 +1,13 @@
-import { Movie } from "@/types";
+import { Movie, MovieResponse } from "@/types";
 import { createContext, ReactNode, useState } from "react";
 
 type MoviesContextData = {
-  movies: Movie[];
-  favoriteMovies: Movie[];
-  addToFavorites: (movie: Movie) => void;
-  removeFromFavorites: (movie: Movie) => void;
+  favoriteMovies: MovieResponse[];
+  addToFavorites: (movie: MovieResponse) => void;
+  removeFromFavorites: (movie: MovieResponse) => void;
 };
 
 export const MoviesContext = createContext<MoviesContextData>({
-  movies: [],
   favoriteMovies: [],
   addToFavorites: () => {},
   removeFromFavorites: () => {}
@@ -20,78 +18,21 @@ type MoviesProviderProps = {
 };
 
 export function MoviesProvider({ children }: MoviesProviderProps) {
-  const movies: Movie[] = [
-    {
-      id: 1,
-      title: "The Shawshank Redemption",
-      overview:
-        "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
-      poster_path: "/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg",
-      release_date: "1994-09-23",
-      vote_average: 9.3,
-    },
-    {
-      id: 2,
-      title: "The Godfather",
-      overview:
-        "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.",
-      poster_path: "/3bhkrj58Vtu7enYsRolD1fZdja1.jpg",
-      release_date: "1972-03-24",
-      vote_average: 9.2,
-    },
-    {
-      id: 3,
-      title: "Pulp Fiction",
-      overview:
-        "The lives of two mob hitmen, a boxer, a gangster and his wife intertwine in four tales of violence and redemption.",
-      poster_path: "/d5iIlFn5s0ImszYzBPb8JPIfbXD.jpg",
-      release_date: "1994-10-14",
-      vote_average: 8.9,
-    },
-    {
-      id: 4,
-      title: "The Dark Knight",
-      overview:
-        "When the menace known as the Joker wreaks havoc on Gotham, Batman must face one of the greatest psychological tests.",
-      poster_path: "/qJ2tW6WMUDux911r6m7haRef0WH.jpg",
-      release_date: "2008-07-18",
-      vote_average: 9.0,
-    },
-    {
-      id: 5,
-      title: "Forrest Gump",
-      overview:
-        "The presidencies of Kennedy and Johnson are shown through the eyes of an Alabama man with an IQ of 75.",
-      poster_path: "/arw2vcBveWOVZr6pxd9XTd1TdQa.jpg",
-      release_date: "1994-07-06",
-      vote_average: 8.8,
-    },
-    {
-      id: 6,
-      title: "Inception",
-      overview:
-        "A skilled thief is given a chance at redemption if he can successfully perform an inception.",
-      poster_path: "/9gk7adHYeDvHkCSEqAvQNLV5Uge.jpg",
-      release_date: "2010-07-16",
-      vote_average: 8.8,
-    },
-  ];
-  const [favoriteMovies, setFavoriteMovies] = useState<Movie[]>([]);
+  const [favoriteMovies, setFavoriteMovies] = useState<MovieResponse[]>([]);
 
-  function addToFavorites(movie: Movie) {
+  function addToFavorites(movie: MovieResponse) {
     setFavoriteMovies([...favoriteMovies, movie]);
   }
 
-  function removeFromFavorites(movie: Movie) {
+  function removeFromFavorites(movie: MovieResponse) {
     setFavoriteMovies(
-      favoriteMovies.filter((favoriteMovie) => favoriteMovie.id !== movie.id)
+      favoriteMovies.filter((favoriteMovie) => favoriteMovie.imdbID !== movie.imdbID)
     );
   }
 
   return (
     <MoviesContext.Provider
       value={{
-        movies: movies,
         favoriteMovies: favoriteMovies,
         addToFavorites: addToFavorites,
         removeFromFavorites: removeFromFavorites,
